aea0ba7d334537cd6fe85f1169f15ce3062da0f4
Clive <clive@reynoldscrc.me.uk>
2019-07-29 16:38:08 +0100

    Re-order app.js

--- app.js
+++ app.js
@@ -24,31 +24,32 @@ require('dotenv').config();
 //Use env variable to connect to database ** set same variable to local or remote db on dev and production environment
 const url = process.env.DATABASEURL || "mongodb://yelpcampUser:password@10.101.202.153:27017/yelpcamp?retryWrites=true&w=majority"; 
 mongoose.connect(url, {useNewUrlParser: true});
-app.use(flash());
-//PASSPORT CONFIG
 
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//PASSPORT CONFIG
 app.use(require("express-session")({
     secret: "I Love Coding",
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.error = req.flash("error");
     res.locals.success = req.flash("success");
     next();
 });
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-//seedDB();
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
